9a32e65678e3e7c42a4cee142f9b91f10e0cfe2d
Ryan Chow <ryanchowdev@gmail.com>
2025-03-13 17:02:18 -0700

    Added feedback to form submission

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,11 @@
 // Form submission
 const onSubmit = async (event) => {
+  // Get data from form, then clear it
   event.preventDefault();
   const formData = new FormData(event.target);
+  event.target.reset();
 
-  // Use public key for form
+  // Use web3forms public key
   formData.append("access_key", "72af4997-1628-4924-82ab-298afa0a9a3f");
 
   const object = Object.fromEntries(formData);
@@ -19,8 +21,9 @@ const onSubmit = async (event) => {
     body: json
   }).then((res) => res.json());
 
+  // Alert user that message was successfully submitted
   if (res.success) {
-    console.log("Success", res);
+    alert("Success! Your message was submitted.");
   }
 };
 
@@ -32,23 +35,22 @@ function Contact () {
             Feel free to contact me with any questions or concerns.<br />
             I will get back to you as soon as possible.<br />
           </p>
-          {/* Contact form 
-          TODO - add visual feedback after submitting form successfully */}
+          {/* Contact form */}
           <form className="d-flex flex-column align-items-center" onSubmit={onSubmit}>
             {/* Name */}
             <div className="mb-3 w-50">
               <label htmlFor="name" className="form-label">Full Name</label>
-              <input type="text" name="name" className="form-control" id="name"/>
+              <input type="text" name="name" className="form-control" id="name" required/>
             </div>
             {/* Email */}
             <div className="mb-3 w-50">
               <label htmlFor="email" className="form-label">Email Address</label>
-              <input type="email" name="email" className="form-control" id="email"/>
+              <input type="email" name="email" className="form-control" id="email" required/>
             </div>
             {/* Message */}
             <div className="mb-3 w-50">
               <label htmlFor="message" className="form-label">Message</label>
-              <textarea className="form-control" name="message" id="message" rows="3"></textarea>
+              <textarea className="form-control" name="message" id="message" rows="3" required></textarea>
             </div>
             {/* Submit */}
             <button type="submit" className="btn btn-success mt-4 py-2 px-3"><i className="bi bi-check-lg"></i> Submit</button>
